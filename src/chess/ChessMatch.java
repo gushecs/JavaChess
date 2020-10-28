32877d0d1db899537c27e8b0280a90c23d8bca41
gushecs <gustavo.heitor@outlook.com>
2020-10-28 19:08:51 -0300

    ChessPosition created

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardGame.Board;
-import boardGame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -24,10 +23,14 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece (char column,int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column,row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.BLACK), new Position(0,0));
-		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(0,7));
+		placeNewPiece('a',8,new Rook(board, Color.BLACK));
+		placeNewPiece('e',8,new King(board, Color.BLACK));
+		placeNewPiece('h',8,new Rook(board, Color.BLACK));
 	}
 	
 }
